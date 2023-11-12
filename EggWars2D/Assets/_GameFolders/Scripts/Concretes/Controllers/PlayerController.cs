c15c0919cec57ab860099ea4597aacbc337ebbe3
berkterek <42406263+berkterek@users.noreply.github.com>
2023-11-12 16:43:31 +0300

    refactor: player can simple move with swipe direction

--- EggWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ EggWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -5,12 +5,28 @@ namespace EggWars2D.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] float _moveSpeed = 10f;
+        [SerializeField] float _minMaxX = 5f;
+        [SerializeField] Transform _transform;
+        
         InputReader _inputReader;
         float _clickedScreenX;
+        float _clickedPlayerX;
+        float _width;
+
+        void OnValidate()
+        {
+            if (_transform == null) _transform = GetComponent<Transform>();
+        }
 
         void Awake()
         {
             _inputReader = new InputReader();
+        }
+
+        void Start()
+        {
+            _width = Screen.width;
         }
 
         void Update()
@@ -23,12 +39,20 @@ namespace EggWars2D.Controllers
             if (_inputReader.IsTouchDown)
             {
                 _clickedScreenX = _inputReader.ScreenTouchPosition.x;
+                _clickedPlayerX = _transform.position.x;
             }
             else if (_inputReader.IsTouch)
             {
                 float xDifference = _inputReader.ScreenTouchPosition.x - _clickedScreenX;
 
-                Debug.Log("X difference => " + xDifference);
+                xDifference /= _width;
+                xDifference *= _moveSpeed;
+
+                float newXPosition = _clickedPlayerX + xDifference;
+                newXPosition = Mathf.Clamp(newXPosition, -_minMaxX, _minMaxX);
+                
+                Vector3 position = _transform.position;
+                _transform.position = new Vector3(newXPosition, position.y, position.z);
             }
         }
     }
