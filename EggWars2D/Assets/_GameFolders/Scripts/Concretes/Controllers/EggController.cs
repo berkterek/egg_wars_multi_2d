af220fd7244fbca0dae2078f47eb4546df698ab8
berkterek <42406263+berkterek@users.noreply.github.com>
2023-11-12 16:55:14 +0300

    feat: egg can bounce on player collider

---
+++ EggWars2D/Assets/_GameFolders/Scripts/Concretes/Controllers/EggController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace EggWars2D.Controllers
+{
+    public class EggController : MonoBehaviour
+    {
+        [SerializeField] Rigidbody2D _rigidbody2D;
+        [SerializeField] float _bounceVelocity = 5f;
+
+        void OnValidate()
+        {
+            if (_rigidbody2D == null) GetComponent<Rigidbody2D>();
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.TryGetComponent(out PlayerController playerController))
+            {
+                Bounce(other.GetContact(0).normal);
+            }
+        }
+
+        void Bounce(Vector3 normal)
+        {
+            _rigidbody2D.velocity = normal * _bounceVelocity;
+        }
+    }
+}
